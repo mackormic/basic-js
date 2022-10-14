39a985e1927c353307eab47304d7b9dafba2ddf1
kenny <k.mackormic@mail.ru>
2022-10-14 20:27:58 +0300

    feat: add dreamTeam and whatSeason

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let alphabet = members.sort();
+  let teamString = alphabet.filter((el) => typeof el === "string");
+  let teamStringUpper = teamString.filter(
+    (el) => el[0] === el[0].toUpperCase()
+  );
+
+  let result = teamStringUpper.map((el) => el[0]);
+  let x = result.join("");
+  return x;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
